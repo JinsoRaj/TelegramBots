d2714bc6a58787f69a0673e737cff8b6547d9227
Jayant Hegde Kageri <jayantkageri@gmail.com>
2022-10-03 23:43:34 +0530

    Add Telegraf Echo Bot (#16)
    * Create index.js
    
    * Create package.json
    
    * Create README.md

---
+++ Nodejs/telegraf/echo-bot/index.js
@@ -0,0 +1,19 @@
+// (c) Telegraf Docs
+
+import { Telegraf, Markup } from "telegraf";
+
+const { BOT_TOKEN } = process.env || "Your BOT TOKEN here";
+if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!');
+const bot = new Telegraf(BOT_TOKEN);
+
+const keyboard = Markup.inlineKeyboard([
+	Markup.button.url("❤️", "http://telegraf.js.org"),
+	Markup.button.callback("Delete", "delete"),
+]);
+
+bot.start(ctx => ctx.reply("Hello"));
+bot.help(ctx => ctx.reply("Help message"));
+bot.on("message", ctx => ctx.copyMessage(ctx.message.chat.id, keyboard));
+bot.action("delete", ctx => ctx.deleteMessage());
+
+bot.launch();
