6dc7d8737f9fc396a399f98f0fbf334492577cb0
CyberNobie <jkarts91@gmail.com>
2022-10-15 11:01:03 +0530

    Added Telegram Bot To Bypass Linkvertise.com Based Shortened Links. (#31)
    * Create README
    
    * Added Gplink Bypasser Bot
    
    Telegram Bot To Bypass Gplink based shortened link
    
    * typo
    
    * init
    
    Telegram Bot to bypass ouo.io based shortened links.
    
    * Update README.md
    
    * Telegram Bot to bypass ouo.io based shortened links.
    
    * Create README.md
    
    * Telegram Bot To Bypass Linkvertise.com Based Shortened Links.

---
+++ Python/Pyrogram/linkvertise_bypasser_bot/bot.py
@@ -0,0 +1,64 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import logging
+from wserver import keep_alive
+from modules.linkvertise import lv_bypass
+logging.basicConfig(level=logging.INFO)
+
+for log_name, log_obj in logging.Logger.manager.loggerDict.items():
+    if log_name != 'pyrogram':
+        log_obj.disabled = True
+
+LOGGER = logging.getLogger(__name__)
+
+API_ID = ' Your telegram API_ID'
+API_HASH = 'Your telegram API_HASH'
+BOT_TOKEN = 'Your telegram BOT_TOKEN'
+
+bot = Client('shortlinkurlbypasserbot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+
+@bot.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    LOGGER.info(f"{message.chat.first_name} Just Started me")
+    await message.reply(f'''**Hi {message.chat.first_name}!**\n'''
+                        '''I'm Linkvertise.com Link Bypasser Bot.\n
+        <b>Send Me Linkvertise.com Shortened Url And I Will Give You Direct Link</b>
+        
+        <b>╭──「⭕️ **Supported Sites** ⭕️」</b>
+        <b>├1️⃣  Linkvertise</b>
+        <b>╰──「🚀 BOT BY @RoarCyber 🚀」</b>
+       ''')
+#Linkvertise Open
+@bot.on_message(filters.regex(r'\bhttps?://.*link\S+') & filters.private)
+async def lv_link_handler(bot, message):
+    url = message.matches[0].group(0)
+    try:
+     await message.reply(f'Pʟᴇᴀsᴇ Wᴀɪᴛ Bʏᴘᴀssɪɴɢ Yᴏᴜʀ Uʀʟ')   
+     bypassed = await lv_bypass(url)
+     BUTTONS = [
+       [
+         InlineKeyboardButton('Oʀɪɢɪɴᴀʟ Uʀʟ', url = url),
+         InlineKeyboardButton('Bʏᴘᴀssᴇᴅ Uʀʟ', url = bypassed)
+       ]
+     ]
+     reply_markup=InlineKeyboardMarkup(BUTTONS)
+     await message.reply(
+       (f'❤️{message.chat.first_name} Yᴏᴜʀ Lɪɴᴋ Hᴀs Bᴇᴇɴ Bʏᴘᴀssᴇᴅ \n 👇 Cʜᴇᴄᴋ Tʜᴇ Bᴜᴛᴛᴏɴs Bᴇʟᴏᴡ\n Powered ʙʏ <b>@RoarCyber</b>'),
+       reply_markup=reply_markup
+     )
+    except Exception as e:
+        LOGGER.error(e)
+        await message.reply(f'Eɴᴄᴏᴜɴᴛᴇʀᴇᴅ ᴀ ᴇʀʀᴏʀ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴛᴏ ᴏᴡɴᴇʀs', quote=True) 
+
+#Linkvertise CLOSED
+
+# ==============================================
+LOGGER.info('I AM ALIVE')
+keep_alive()
+bot.run()
