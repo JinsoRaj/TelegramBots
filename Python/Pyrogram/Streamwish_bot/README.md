dbdb23a5741ee077c29ee24fb423df0942537a00
CyberNobie <jkarts91@gmail.com>
2023-10-26 11:56:04 +0530

    Streamwish Bot (#39)
    * Streamwish Bot
    
    hacktoberfest2023
    
    * Create requirements.txt
    
    * Create main.py
    
    * Update README.md

---
+++ Python/Pyrogram/Streamwish_bot/README.md
@@ -0,0 +1,6 @@
+# Streamwish Bot
+
+Usage
+```shell
+python main.py
+```
---
+++ Python/Pyrogram/Streamwish_bot/main.py
@@ -0,0 +1,71 @@
+#####################################
+##                                 ##
+##      Work By CyberNobie         ##
+##  https://github.com/cybernobie  ##
+##                                 ##
+#####################################
+import requests
+import os
+from pyrogram import Client, filters
+import time
+
+BASE_URL = "https://api.streamwish.com/api"  # StreamWish API base URL
+API_KEY = ""  # Replace with your StreamWish API key/token
+# Define headers with API key
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json",
+}
+
+# Your Telegram API ID
+API_ID = ''
+# Your Telegram API HASH
+API_HASH = ''
+# Your Telegram BOT token
+BOT_TOKEN = ''
+# List of approved user IDs
+approved_users = [1010583341] # [ ID1 , ID2 , ..]
+# Create the Pyrogram client
+app = Client("streamflix_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Account Information
+def get_account_info():
+    response = requests.get(f"{BASE_URL}/account/info?key={API_KEY}", headers=headers)
+    data_ac_info=response.json()
+    return data_ac_info
+
+   
+
+@app.on_message(filters.command("start"))
+def start(bot, message):
+    bot.send_message(message.chat.id, 'Welcome to the Streamflix Bot! Use /help for more')
+
+@app.on_message(filters.command("help"))
+def help(bot,message):
+    bot.send_message(message.chat.id, 'User Commands\n/accountinfo - Account Information \n\nBot By @RoarCyber')
+
+# account info open  
+@app.on_message(filters.command("accountinfo"))
+def accountinfo(bot,message):
+    if message.from_user.id not in approved_users:
+        bot.send_message(message.chat.id, "You are not an approved user.")
+        return
+    else:
+        clear_screen()
+        account_info = get_account_info()
+        Storage_KB = account_info["result"]["storage_used"]
+        storage_info = Storage_KB / 1000000
+        bot.send_message(message.chat.id, f'<b>Showing Account Information:</b>\n\nUsername: {account_info["result"]["login"]}\nPremium Expire: {account_info["result"]["premium_expire"]}\nEmail: {account_info["result"]["email"]}\nTotal Files: {account_info["result"]["files_total"]}\nPremium: {account_info["result"]["premium"]}\nBalance: {account_info["result"]["balance"]}\nStorage Used: {storage_info:.2f} GB\nStorage Left: {account_info["result"]["storage_left"]}\nServer Time: {account_info["server_time"]}')
+
+# account info closed 
+
+
+
+
+ 
+
+# Run the bot
+app.run()
