c9db09924f52dc4829780aff0ce4a2390d66fd13
Alex <sashavalera35@gmail.com>
2024-11-04 12:42:13 +0800

    Add 'get_current_time_y_m_d' to request current date from the NTP server

--- Python/AIOGram/VideoUploaderBot/src/uploading_timer.py
+++ Python/AIOGram/VideoUploaderBot/src/uploading_timer.py
@@ -22,3 +22,16 @@ class Timer:
             logger.error("Connection or Timeout error occurred while trying to reach the NTP server.")
         except Exception as e:
             logger.critical("An unexpected error occurred: %s", e)
+
+    def get_current_time_y_m_d(self):
+        try:
+            c = ntplib.NTPClient()
+            response = c.request('pool.ntp.org')
+            time_object = datetime.fromtimestamp(response.tx_time, timezone.utc)
+            current_time = time_object.strftime("%Y:%m:%d")
+            logger.info("Successfully requested current time from the ntp server.")
+            return current_time
+        except TimeoutError or ConnectionError:
+            logger.error("Connection or Timeout error occurred while trying to reach the NTP server.")
+        except Exception as e:
+            logger.critical("An unexpected error occurred: %s", e)
