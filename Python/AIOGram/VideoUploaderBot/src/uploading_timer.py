35ab804810e38976ade98161a79fd78691defd64
Alex <sashavalera35@gmail.com>
2024-11-04 12:38:58 +0800

    Add 'Timer' class to request current time from the NTP server

---
+++ Python/AIOGram/VideoUploaderBot/src/uploading_timer.py
@@ -0,0 +1,24 @@
+import ntplib
+import datetime
+
+from datetime import datetime, timezone
+from __init__ import logger
+
+
+class Timer:
+    def __init__(self):
+        self.ntp_server = 'pool.ntp.org'
+        self.client = ntplib.NTPClient()
+
+    def get_current_time_h_m_s(self):
+        try:
+            c = ntplib.NTPClient()
+            response = c.request('pool.ntp.org')
+            time_object = datetime.fromtimestamp(response.tx_time, timezone.utc)
+            current_time = time_object.strftime("%H:%M:%S")
+            logger.info("Successfully requested current time from the ntp server.")
+            return current_time
+        except TimeoutError or ConnectionError:
+            logger.error("Connection or Timeout error occurred while trying to reach the NTP server.")
+        except Exception as e:
+            logger.critical("An unexpected error occurred: %s", e)
