f60c481f1c94fae1e39cf1433ceca1bd2848f32b
Alex <sashavalera35@gmail.com>
2024-11-04 12:32:54 +0800

    Add methods to create and validate the format of the schedule. Add method 'get_users_to_upload' and 'log_update' for managing videos uploading

--- Python/AIOGram/VideoUploaderBot/src/user_data_handler.py
+++ Python/AIOGram/VideoUploaderBot/src/user_data_handler.py
@@ -165,3 +165,98 @@ class DataHandler:
             logger.critical(f"An unexpected error occurred: {e}")
             return False
 
+    async def setup_user_schedule(self, user_id, daily_upload_times=None):
+        if daily_upload_times is not None:
+            daily_times = []
+            for time_str in daily_upload_times:
+                if not await self.date_time_validation(time_str):
+                    return False
+                daily_times.append(time_str)
+            if await self.update_user(user_id=user_id, table_name='daily_upload_times', upload_time=daily_times):
+                return True
+            else:
+                return False
+
+    async def get_users_to_upload(self, now_time, now_date):
+        try:
+            async with aiosqlite.connect(self.database_name) as conn:
+                cursor = await conn.cursor()
+
+                current_time = now_time
+                today_date = now_date
+
+                query = '''
+                SELECT DISTINCT dt.user_id
+                FROM daily_upload_times dt
+                LEFT JOIN upload_log ul
+                ON dt.user_id = ul.user_id AND dt.upload_time = ul.upload_time AND ul.date = ?
+                WHERE dt.upload_time = ? AND ul.id IS NULL
+                '''
+
+                result = await cursor.execute(query, (today_date, current_time))
+                users_to_upload = await result.fetchall()
+
+                logger.info(f"Found {len(users_to_upload)} users scheduled for upload at {current_time}.")
+                return [user[0] for user in users_to_upload]
+
+        except aiosqlite.Error as e:
+            logger.error(f"Failed to fetch users to upload: {e}")
+            return []
+
+        except Exception as e:
+            logger.critical(f"An unexpected error occurred: {e}")
+            return []
+
+    async def log_upload(self, user_id, upload_time):
+        try:
+            async with aiosqlite.connect(self.database_name) as conn:
+                cursor = await conn.cursor()
+
+                today_date = date.today().isoformat()
+
+                await cursor.execute('''
+                INSERT OR IGNORE INTO upload_log (user_id, upload_time, date)
+                VALUES (?, ?, ?)
+                ''', (user_id, upload_time, today_date))
+
+                await conn.commit()
+                logger.info(f"Logged upload for user {user_id} at {upload_time} on {today_date}.")
+                return True
+
+        except aiosqlite.Error as e:
+            logger.error(f"Failed to log upload for user {user_id}: {e}")
+            return False
+
+        except Exception as e:
+            logger.critical(f"An unexpected error occurred: {e}")
+            return False
+
+    async def get_user_api(self, user_id):
+        try:
+            async with aiosqlite.connect(self.database_name) as conn:
+                cursor = await conn.cursor()
+                query = '''
+                    SELECT api_key
+                    FROM users_data
+                    WHERE user_id = ?
+                    '''
+
+                result = await cursor.execute(query, (user_id,))
+                api_key = await result.fetchone()
+
+                logger.info(f"User {api_key} successfully retrieved.")
+
+        except aiosqlite.Error as e:
+            logger.error(f"Failed to log upload for user {user_id}: {e}")
+            return False
+
+        except Exception as e:
+            logger.critical(f"An unexpected error occurred: {e}")
+            return False
+
+    async def date_time_validation(self, date_time_str):
+        time_pattern = r'^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$'
+        if re.match(time_pattern, date_time_str):
+            return True
+        return False
+
