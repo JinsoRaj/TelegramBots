c6d1a1df1df0378fcfdcfd7c43aa383d063b504c
Alex <sashavalera35@gmail.com>
2024-11-05 23:07:29 +0800

    Add creation of videos anf text files folders

--- Python/AIOGram/VideoUploaderBot/src/__init__.py
+++ Python/AIOGram/VideoUploaderBot/src/__init__.py
@@ -3,9 +3,18 @@ import os
 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 logs_path = os.path.join(project_root, 'logs')
+videos_path = os.path.join(project_root, 'videos_path')
+text_files = os.path.join(project_root, 'text_files')
+
 
 if not os.path.exists(logs_path):
     os.makedirs(logs_path)
+
+if not os.path.exists(videos_path):
+    os.makedirs(videos_path)
+
+if not os.path.exists(text_files):
+    os.makedirs(text_files)
 
 logging.basicConfig(
     filename=os.path.join(logs_path, 'app.log'),
