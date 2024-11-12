cad0ab293a24fda297463eba0568c2afb4726979
Yehor Hiba <20128413@tafe.wa.edu.au>
2024-11-12 09:39:53 +0800

    feat: Added main keyboard layout for the bot.

--- Python/liquidation-tracker-bot/src/bot.py
+++ Python/liquidation-tracker-bot/src/bot.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import CommandStart
 
 load_dotenv()
@@ -14,12 +14,25 @@ dp = Dispatcher()
 
 TELEGRAM_CHAT_ID = None
 
+def main_keyboard():
+    kb = [
+        [
+            KeyboardButton(text="Binance Liquidations"),
+            KeyboardButton(text="BYBIT Liquidations")
+        ],
+        [
+            KeyboardButton(text="Settings"),
+            KeyboardButton(text="Stop")
+        ],
+    ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
     global TELEGRAM_CHAT_ID
     TELEGRAM_CHAT_ID = message.chat.id
 
-    await message.answer(f"Hi, {message.from_user.full_name}! I'm a Liquidation Notifier Bot.")
+    await message.answer(f"Hi, {message.from_user.full_name}! I'm a Liquidation Notifier Bot.", reply_markup=main_keyboard())
 
 async def main():
     await dp.start_polling(bot)
